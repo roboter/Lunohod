f029dedc83b0a95c0511e667025c4d46e1f2d6e2
Robby Roboter <roboter@begemotik.ee>
2014-10-20 21:58:24 +0300

    melody as a function

--- Melody/Melody.ino
+++ Melody/Melody.ino
@@ -25,25 +25,37 @@ int melody[] = {
 int noteDurations[] = {
   4, 8, 8, 4,4,4,4,4 };
 
+int tonePin =8;
 void setup() {
-  // iterate over the notes of the melody:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  playMelody();
+  // no need to repeat the melody.
+  Serial.println("Hello World");
+  delay(1000);
+  
+  
+}
+
+void playMelody()
+{
+    // iterate over the notes of the melody:
   for (int thisNote = 0; thisNote < 8; thisNote++) {
 
     // to calculate the note duration, take one second
     // divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
     int noteDuration = 1000/noteDurations[thisNote];
-    tone(8, melody[thisNote],noteDuration);
+    tone(tonePin, melody[thisNote],noteDuration);
 
     // to distinguish the notes, set a minimum time between them.
     // the note's duration + 30% seems to work well:
     int pauseBetweenNotes = noteDuration * 1.30;
     delay(pauseBetweenNotes);
     // stop the tone playing:
-    noTone(8);
+    noTone(tonePin);
   }
-}
-
-void loop() {
-  // no need to repeat the melody.
 }
